010f9fa466fbc20ee5bb7a97dcbd57ca43267f28
RTAkland <me@rtast.cn>
2024-09-06 15:28:48 +0800

    fix: fix weather null pointer tip

--- src/main/kotlin/cn/rtast/fancybot/commands/WeatherCommand.kt
+++ src/main/kotlin/cn/rtast/fancybot/commands/WeatherCommand.kt
@@ -28,24 +28,32 @@ class WeatherCommand : BaseCommand() {
             listener.sendGroupMessage(message.groupId, msg)
             return
         }
-        val locationName = args.first()
-        val lookupLocationId = Http.get<Geo>(
-            "https://geoapi.qweather.com/v2/city/lookup",
-            mapOf("location" to locationName, "key" to configManager.qweatherKey)
-        ).location.first().id
-        val weather = Http.get<Weather>(
-            "https://devapi.qweather.com/v7/weather/now",
-            mapOf("location" to lookupLocationId, "key" to configManager.qweatherKey)
-        )
-        val msg = MessageChain.Builder()
-            .addAt(message.sender.userId)
-            .addNewLine()
-            .addText("城市: $locationName, 温度: ${weather.now.temp} ℃")
-            .addNewLine()
-            .addText("${weather.now.text}/${weather.now.windDir}")
-            .addNewLine()
-            .addText("数据来源: ${weather.refer.sources.joinToString(",")}")
-            .build()
-        listener.sendGroupMessage(message.groupId, msg)
+        try {
+            val locationName = args.first()
+            val lookupLocation = Http.get<Geo>(
+                "https://geoapi.qweather.com/v2/city/lookup",
+                mapOf("location" to locationName, "key" to configManager.qweatherKey)
+            ).location.first()
+            val weather = Http.get<Weather>(
+                "https://devapi.qweather.com/v7/weather/now",
+                mapOf("location" to lookupLocation.id, "key" to configManager.qweatherKey)
+            )
+            val msg = MessageChain.Builder()
+                .addAt(message.sender.userId)
+                .addNewLine()
+                .addText("城市: ${lookupLocation.name}, 温度: ${weather.now.temp} ℃")
+                .addNewLine()
+                .addText("${weather.now.text}/${weather.now.windDir}")
+                .addNewLine()
+                .addText("数据来源: ${weather.refer.sources.joinToString(",")}")
+                .build()
+            listener.sendGroupMessage(message.groupId, msg)
+        } catch (_: NullPointerException) {
+            val msg = MessageChain.Builder()
+                .addAt(message.sender.userId)
+                .addText("没有查询到该城市的天气信息哦~")
+                .build()
+            listener.sendGroupMessage(message.groupId, msg)
+        }
     }
 }
