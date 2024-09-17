1429b6e3237da2162948ae8e6b822f3d8afbde0d
RTAkland <me@rtast.cn>
2024-09-17 12:45:43 +0800

    chore: set text padding

--- src/main/kotlin/cn/rtast/fancybot/commands/lookup/DomainPriceCommand.kt
+++ src/main/kotlin/cn/rtast/fancybot/commands/lookup/DomainPriceCommand.kt
@@ -49,8 +49,8 @@ class DomainPriceCommand : BaseCommand() {
         g2d.font = font
         g2d.color = textColor
         g2d.drawString("域名后缀: $suffix 首年注册价格: ${domainPrice.first}元", 50, 130)
-        g2d.drawString("续费价格: ${domainPrice.second}元", 50, 150)
-        g2d.drawString("数据来源: 腾讯云", 50, 170)
+        g2d.drawString("续费价格: ${domainPrice.second}元", 50, 160)
+        g2d.drawString("数据来源: 腾讯云", 50, 190)
         g2d.dispose()
         return canvas.toByteArray().encodeToBase64()
     }
@@ -66,10 +66,10 @@ class DomainPriceCommand : BaseCommand() {
         g2d.font = font
         g2d.color = textColor
         g2d.drawString("域名$domain${if (available) "可以注册" else "已被注册"}", 50, 110)
-        g2d.drawString("${if (premium) "是" else "不是"}白金域名", 50, 130)
-        g2d.drawString("首年年注册价格: ${domainPrice.first}元", 50, 150)
-        g2d.drawString("续费价格: ${domainPrice.second}元", 50, 170)
-        g2d.drawString("数据来源: 腾讯云", 50, 190)
+        g2d.drawString("${if (premium) "是" else "不是"}白金域名", 50, 140)
+        g2d.drawString("首年年注册价格: ${domainPrice.first}元", 50, 170)
+        g2d.drawString("续费价格: ${domainPrice.second}元", 50, 200)
+        g2d.drawString("数据来源: 腾讯云", 50, 230)
         g2d.dispose()
         return canvas.toByteArray().encodeToBase64()
     }
